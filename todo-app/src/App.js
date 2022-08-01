aab8b7930ec02d878084a4c8cc6aba7308305a21
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-01 09:42:31 +0530

    reusing the same component

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,20 +1,13 @@
+import Todo from "./Components/Todo";
+
 function App() {
   return (
     <>
       <div>
         <h1>To Do Application</h1>
-        <div className="card">
-          <h1>Title</h1>
-          <div className="actions">
-            <button className="btn">Delete</button>
-          </div>
-        </div>
-        <div className="card">
-          <h1>Title</h1>
-          <div className="actions">
-            <button className="btn">Delete</button>
-          </div>
-        </div>
+        <Todo/>
+        <Todo/>
+        <Todo/>
       </div>
     </>
   );
---
+++ todo-app/src/Components/Todo.js
@@ -0,0 +1,12 @@
+function Todo() {
+    return(
+        <div className="card">
+            <h1>Title</h1>
+            <div className="actions">
+                <button className="btn">Delete</button>
+            </div>
+        </div>
+    )
+}
+
+export default Todo
