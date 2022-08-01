b69e4d22d7b9c5f5179ed5f1b4795ce0db5e8853
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-01 09:50:59 +0530

    handling events for delete btn

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,14 +2,12 @@ import Todo from "./Components/Todo";
 
 function App() {
   return (
-    <>
       <div>
         <h1>To Do Application</h1>
         <Todo title="Learn ReactJS"/>
         <Todo title="Learn NodeJs"/>
         <Todo title="Learn NextJS"/>
       </div>
-    </>
   );
 }
 
--- todo-app/src/Components/Todo.js
+++ todo-app/src/Components/Todo.js
@@ -1,9 +1,14 @@
 function Todo(props) {
+    const handleDelete = function(){
+        console.log(props.title)
+        console.log("Working button")
+    }
+
     return(
         <div className="card">
             <h1>{props.title}</h1>
             <div className="actions">
-                <button className="btn">Delete</button>
+                <button className="btn" onClick={handleDelete}>Delete</button>
             </div>
         </div>
     )
