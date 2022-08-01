eadafc7e48802a410d44723cac0345986b22e535
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-01 17:30:17 +0530

    Scoping component styles with css classes

--- meetup-app/src/Components/Layout/MainNavigation.js
+++ meetup-app/src/Components/Layout/MainNavigation.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import classes from './MainNavigation.module.css'
 
 function MainNavigation() {
   return (
-    <>
-        <div>React Meetups</div>
+    <div className={classes.header}>
+        <div className={classes.logo}>React Meetups</div>
         <nav>
             <ul>
                 <li><Link to='/'>All Meetups</Link></li>
@@ -12,7 +13,7 @@ function MainNavigation() {
                 <li><Link to='/favourites'>Favourites</Link></li>
             </ul>
         </nav>
-    </>
+    </div>
   )
 }
 
