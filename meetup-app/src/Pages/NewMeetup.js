91b6f03ca4fdf9d2db7f15c661d98ea8b2460fe7
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-02 09:00:52 +0530

    create data fetching mechanism and use useEffect

--- meetup-app/src/Pages/NewMeetup.js
+++ meetup-app/src/Pages/NewMeetup.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import NewMeetupForm from '../Components/Meetup/NewMeetupForm'
 
 function NewMeetup() {
+  const navigate = useNavigate();
   function handleMeetupData(meetup){
+
     fetch(
       'https://react-meetup-app-bc958-default-rtdb.firebaseio.com/meetups.json',
       {
@@ -12,7 +15,10 @@ function NewMeetup() {
           'Content-Type' : 'application/json'
         }
       }
-    )
+    ).then(()=>{
+      navigate('/')
+    })
+
   }
 
   return (
