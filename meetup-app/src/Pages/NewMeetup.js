4c0167893bc0601ba99622888806aa69d9fc5555
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-02 07:51:56 +0530

    create a post request to the firebase

--- meetup-app/src/Pages/NewMeetup.js
+++ meetup-app/src/Pages/NewMeetup.js
@@ -2,8 +2,21 @@ import React from 'react'
 import NewMeetupForm from '../Components/Meetup/NewMeetupForm'
 
 function NewMeetup() {
+  function handleMeetupData(meetup){
+    fetch(
+      'https://react-meetup-app-bc958-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method : 'POST',
+        body : JSON.stringify(meetup),
+        headers : {
+          'Content-Type' : 'application/json'
+        }
+      }
+    )
+  }
+
   return (
-    <NewMeetupForm/>
+    <NewMeetupForm onAddMeetup={handleMeetupData}/>
   )
 }
 
